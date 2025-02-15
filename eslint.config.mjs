3c162a2713b0d31942ae2ad59e46ac8008052c26
Muhammad Dyas Yaskur <dyas@yaskur.com>
2025-02-16 02:52:51 +0700

    ci: fix pnpm setup in GitHub Actions (#1)
    * ci: fix pnpm setup in GitHub Actions
    
    * Update lint.yml
    
    * Update lint.yml
    
    * chore: rename eslint config
    
    * chore: rename eslint config and ignore dist
    
    * chore: add @eslint/js
    
    * show installed module
    
    * ci: only run in main branch
    
    * ci: ignore pnpm-lock.yaml

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// eslint.config.mjs
+import js from '@eslint/js'
+import ts from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+export default [
+  {
+    ignores: ['dist/', 'tests/', 'pnpm-lock.yaml'], // Ignore the dist folder
+  },
+  js.configs.recommended, // Base ESLint rules
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        console: 'readonly', // Allow console usage
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': ['error', {bracketSpacing: false, printWidth: 120}],
+      '@typescript-eslint/no-unused-vars': ['warn', {argsIgnorePattern: '^_'}],
+      'object-curly-spacing': ['error', 'never'],
+      'max-len': ['warn', {code: 120, ignoreStrings: true, ignoreTemplateLiterals: true}],
+    },
+  },
+  prettierConfig, // Apply Prettier last to avoid conflicts
+]
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,31 @@
 import {HttpFunction} from '@google-cloud/functions-framework'
 import {ChatEvent} from './types/event'
 import {createMessageResponse} from './utils'
+import * as fs from 'node:fs'
 
 export const app: HttpFunction = async (req, res) => {
   if (!(req.method === 'POST' && req.body)) {
     console.log('unknown access', req.hostname, req.ips.join(','), req.method, JSON.stringify(req.body))
-    res.status(400).send('')
-  }
-  const event: ChatEvent = req.body
-  if (event) {
-    console.log(JSON.stringify(event))
-  }
+    fs.readdir('./node_modules', (err, files) => {
+      if (err) {
+        res.status(500).send('Error reading node_modules')
+      } else {
+        console.log(files.join('\n'))
+        res.status(200).send(files.join('\n'))
+      }
+    })
+    // res.status(400).send('')
+  } else {
+    const event: ChatEvent = req.body
 
-  if (event.chat.addedToSpacePayload) {
-    res.json(createMessageResponse('Hi, thanks for install my app' ))
+    if (event) {
+      console.log(JSON.stringify(event))
+    }
+
+    if (event.chat.addedToSpacePayload) {
+      res.json(createMessageResponse('Hi, thanks for install my app'))
+    } else {
+      res.json(createMessageResponse('Hi, what can I do for you?'))
+    }
   }
-
-  res.json(createMessageResponse('Hi, what can I do for you?'))
-
 }
-
-
